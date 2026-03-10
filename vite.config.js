a32d5e8c2b94b2562cbe448a4cbedc7efc93f1e2
udaya <udayabca2021@gmail.com>
2026-03-10 23:26:23 +0530

    render

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: 'all',
+    allowedHosts: ['rvscas-portal.onrender.com', 'localhost', '127.0.0.1'],
     proxy: {
       '/api': {
         target: 'http://localhost:5006',
